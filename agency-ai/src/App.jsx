eb95351501da842ec3fa8d18afee836fa0a23720
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-21 11:42:50 +0530

    app.jsx file add routing paths

--- agency-ai/src/App.jsx
+++ agency-ai/src/App.jsx
@@ -4,9 +4,9 @@ import Navbar from './components/Navbar'
 const App = () => {
 
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
-  
+
   return (
-    <div className="relative min-h-screen bg-white dark:bg-black">
+    <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
