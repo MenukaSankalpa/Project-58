470b5a8ff3b56769d2372e1f1d825297096e007e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-22 15:26:38 +0530

    app.jsx file updated

--- agency-ai/src/App.jsx
+++ agency-ai/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
@@ -13,6 +13,15 @@ const App = () => {
 
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
 
+  const dotRef = useRef(null)
+  const outlineRef = useRef(null)
+
+  // refs for custom cursor position tracking
+  const mouse = useRef({x: 0, y: 0})
+  const position = useRef({x: 0, y: 0})
+
+  
+
   return (
     <div className='dark:bg-black relative'>
       <Toaster />
@@ -24,6 +33,16 @@ const App = () => {
       <Teams />
       <ContactUs />
       <Footer theme={theme}/>
+    
+    {/* Custom cursor ring */}
+    <div  ref={outlineRef} className='fixed top-0 left-0 w-10 rounded-full
+    border border-primary pointer-events-none z-[9999]'></div>
+    
+    {/* Custom cursor dot */}
+    <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full
+    bg-primary pointer-events-none z-[9999]'></div>
+
+
     </div>
   )
 }
