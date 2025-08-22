f9f7dc6d5246c1899840410c04a62504ffe0c2df
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-22 16:08:08 +0530

    App.jsx file add mouse cursor

--- agency-ai/src/App.jsx
+++ agency-ai/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
@@ -20,7 +20,32 @@ const App = () => {
   const mouse = useRef({x: 0, y: 0})
   const position = useRef({x: 0, y: 0})
 
-  
+  useEffect(()=>{
+    const handleMouseMove = (e) =>{
+      mouse.current.x = e.clientX
+      mouse.current.y = e.clientY
+    }
+
+    document.addEventListener('mousemove', handleMouseMove)
+
+    const animate = () => {
+      position.current.x += (mouse.current.x - position.current.x) * 0.1
+      position.current.y += (mouse.current.y - position.current.y) * 0.1
+
+      if(dotRef.current && outlineRef.current){
+        dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y -6}px, 0)`
+        outlineRef.current.style.transform = `translate3d(${position.current.x - 20}px, ${position.current.y - 20}px, 0)`
+      }
+      requestAnimationFrame(animate)
+    }
+    animate()
+    
+    return ()=> {
+      document.removeEventListener('mousemove', handleMouseMove)
+    }
+
+
+  }, [])
 
   return (
     <div className='dark:bg-black relative'>
@@ -35,8 +60,8 @@ const App = () => {
       <Footer theme={theme}/>
     
     {/* Custom cursor ring */}
-    <div  ref={outlineRef} className='fixed top-0 left-0 w-10 rounded-full
-    border border-primary pointer-events-none z-[9999]'></div>
+    <div  ref={outlineRef} className='fixed top-0 left-0 w-10 
+    rounded-full border border-primary pointer-events-none z-[9999]'></div>
     
     {/* Custom cursor dot */}
     <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full
