0d76f8f671842d06ba93bd13c543d72825ac7dae
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-23 20:15:55 +0530

    motion added

--- agency-ai/src/components/ContactUs.jsx
+++ agency-ai/src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast'
+import { motion } from 'motion/react'
 
 const ContactUs = () => {
 
@@ -30,15 +31,23 @@ const ContactUs = () => {
             toast.error(error.message)
         }
     }
-
   return (
-    <div className='flex flex-col items-center
+    <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{ once: true }}
+    transition={{ staggerChildren: 0.2 }}
+    className='flex flex-col items-center
     gp-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
-        
         <Title title='Reach out to us' desc='From strategy to 
         execution, we craft digital solutions that move your business forward'/>
         
-        <form onSubmit={onSubmit} action="" className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+        <motion.form
+        initial={{opacity: 0, y: 30}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.4}}
+        viewport={{once: true}}
+        onSubmit={onSubmit}  action="" className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
 
             <div>
                 <p className='mb-2 text-sm font-medium'>Your Name</p>
@@ -62,7 +71,7 @@ const ContactUs = () => {
 
             <div className='sm:col-span-2'>
                 <p className='mb-2 text-sm font-medium'>Message</p>
-                <textarea rows={8} placeholder='Enter your message' name="message" className='w-full
+                <textarea name="message" rows={8} placeholder='Enter your message' className='w-full
                 p-3 text-sm outline-none rounded-lg border border-gray-300
                 dark:border-gray-600' required/>
             </div>
@@ -73,10 +82,9 @@ const ContactUs = () => {
                 Submit <img src={assets.arrow_icon} alt="" />
             </button>
 
-        </form>
-    </div>
+        </motion.form>
+    </motion.div>
   )
 }
 
-export default ContactUs
-
+export default ContactUs;
